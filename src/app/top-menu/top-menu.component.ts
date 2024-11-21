d4fb9679a2696aa977a3c6ef7137ae51ef2c2b52
Antonio Esclapez <titoway33@gmail.com>
2024-11-21 11:12:22 +0100

    updated imports

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'top-menu',
   standalone: true,
-  imports: [RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './top-menu.component.html',
   styleUrl: './top-menu.component.css',
 })
