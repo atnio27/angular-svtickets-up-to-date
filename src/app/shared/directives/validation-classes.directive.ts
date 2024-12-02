153f0e06a0be545d2ccf0667ba3384cf93912678
Antonio Esclapez <titoway33@gmail.com>
2024-12-02 12:31:34 +0100

    Adapted validation class to be compatible with reactive forms

--- src/app/shared/directives/validation-classes.directive.ts
+++ src/app/shared/directives/validation-classes.directive.ts
@@ -2,26 +2,41 @@ import {
   computed,
   Directive,
   inject,
+  Injector,
   input,
+  OnInit,
+  Signal,
   signal,
   untracked,
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { NgModel } from '@angular/forms';
+import { NgControl, NgModel } from '@angular/forms';
+import { EMPTY } from 'rxjs';
 
 @Directive({
-  selector: '[validationClasses][ngModel]',
+  selector:
+    '[validationClasses][ngModel],[validationClasses][formControl],[validationClasses][formControlName])',
   standalone: true,
   host: {
     '[class]': 'inputClass()',
     '(blur)': 'touched.set(true)',
   },
 })
-export class ValidationClassesDirective {
-  #ngModel = inject(NgModel);
-  validationClasses = input.required<{ valid: string; invalid: string }>();
-  valueChanges = toSignal(this.#ngModel.valueChanges!);
+export class ValidationClassesDirective implements OnInit {
+  #ngModel = inject(NgModel, { optional: true }); // Formulario de plantilla
+  #ngControl = inject(NgControl, { optional: true }); // Formulario reactivo
+  #injector = inject(Injector);
+  validationClasses = input<{ valid: string; invalid: string }>();
+  valueChanges!: Signal<string>;
   touched = signal(false);
+
+  ngOnInit(): void {
+    // La directiva NgControl no está lista hasta este momento del ciclo de vida
+    this.valueChanges = toSignal(
+      this.#ngModel?.valueChanges ?? this.#ngControl?.valueChanges ?? EMPTY,
+      { injector: this.#injector }
+    );
+  }
 
   inputClass = computed(() => {
     const touched = this.touched();
@@ -30,9 +45,9 @@ export class ValidationClassesDirective {
 
     return untracked(() => {
       if (touched) {
-        return this.#ngModel.invalid
-          ? validationClasses.invalid
-          : validationClasses.valid;
+        return this.#ngModel?.invalid || this.#ngControl?.invalid
+          ? validationClasses?.invalid
+          : validationClasses?.valid;
       }
       return '';
     });
