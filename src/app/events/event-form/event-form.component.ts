c35a4e045adabe2887be6e73c77076ec4f2ccbf1
Antonio Esclapez <titoway33@gmail.com>
2024-12-02 12:32:42 +0100

    Lot of changes for reactive forms

--- src/app/events/event-form/event-form.component.ts
+++ src/app/events/event-form/event-form.component.ts
@@ -1,17 +1,22 @@
 import { Component, DestroyRef, inject } from '@angular/core';
 import {
-  FormControl,
-  FormGroup,
+  AbstractControl,
   FormsModule,
+  NonNullableFormBuilder,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
 } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
 import { EncodeBase64Directive } from '../../shared/directives/encode-base64.directive';
 import { EventsService } from '../services/events.service';
 import { ValidationClassesDirective } from '../../shared/directives/validation-classes.directive';
-import { MinDateDirective } from '../../shared/directives/min-date.directive';
 import { DatePipe } from '@angular/common';
+import { MyEvent } from '../../shared/interfaces/my-event';
+import { CanComponentDeactivate } from '../../shared/guards/leave-page-guard.guard';
+import { minDateValidator } from '../../shared/directives/min-date.validator';
 
 @Component({
   selector: 'event-form',
@@ -20,20 +25,29 @@ import { DatePipe } from '@angular/common';
     FormsModule,
     EncodeBase64Directive,
     ValidationClassesDirective,
-    MinDateDirective,
     DatePipe,
     ReactiveFormsModule,
   ],
   templateUrl: './event-form.component.html',
   styleUrl: './event-form.component.css',
 })
-export class EventFormComponent {
-  eventForm = new FormGroup({
-    title: new FormControl(''),
-    description: new FormControl(''),
-    price: new FormControl(0),
-    image: new FormControl(''),
-    date: new FormControl(''),
+export class EventFormComponent implements CanComponentDeactivate {
+  #fb = inject(NonNullableFormBuilder);
+  todayDate: string = new Date().toISOString().split('T')[0];
+
+  eventForm = this.#fb.group({
+    title: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern('^[a-zA-Z][a-zA-Z ]*$'),
+      ],
+    ],
+    description: ['', Validators.required],
+    price: [0, [Validators.required, Validators.min(0)]],
+    image: ['', Validators.required],
+    date: ['', [Validators.required, minDateValidator(this.todayDate)]],
   });
 
   imageBase64 = '';
@@ -43,19 +57,24 @@ export class EventFormComponent {
   #router = inject(Router);
 
   saved = false;
-  minDateToCheck: string = new Date().toISOString().split('T')[0];
 
-  newEvent = {
-    title: '',
-    description: '',
-    price: 0,
-    image: '',
-    date: '',
-  };
+  // TEMPLATE DRIVEN
+  // newEvent = {
+  //   title: '',
+  //   description: '',
+  //   price: 0,
+  //   image: '',
+  //   date: '',
+  // };
 
   addEvent() {
+    const event: MyEvent = {
+      ...this.eventForm.getRawValue(),
+      image: this.imageBase64,
+    };
+
     this.#eventsService
-      .addEvent(this.newEvent)
+      .insertEvent(event)
       .pipe(takeUntilDestroyed(this.#destroyRef))
       .subscribe(() => {
         this.saved = true;
@@ -65,14 +84,25 @@ export class EventFormComponent {
 
   checkImage(fileInput: HTMLInputElement) {
     if (!fileInput.files || fileInput.files.length === 0) {
-      this.newEvent.image = '';
+      // this.newEvent.image = ''; // TEMPLATE DRIVEN
+      this.imageBase64 = '';
     }
   }
 
   canDeactivate() {
     return (
       this.saved ||
+      this.eventForm.pristine ||
       confirm('Do you want to leave the page? Changes will be lost...')
     );
+  }
+
+  minDateValidator(minDate: string): ValidatorFn {
+    return (c: AbstractControl): ValidationErrors | null => {
+      if (c.value && minDate && minDate > c.value) {
+        return { minDate: true };
+      }
+      return null;
+    };
   }
 }
