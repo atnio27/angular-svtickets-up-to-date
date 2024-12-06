5ddbe1528b11732029c545f5f098bcdccc53ca03
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-06 19:12:26 +0100

    Added basic validation for login-page

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ValidationClassesDirective } from '../../shared/directives/validation-classes.directive';
 
 @Component({
   selector: 'login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ValidationClassesDirective],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  #fb = inject(NonNullableFormBuilder);
 
+  loginForm = this.#fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 }
