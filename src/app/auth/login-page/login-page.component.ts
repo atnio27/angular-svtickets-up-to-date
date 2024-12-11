d5cdc88bcdef25b8f2f86d1dbe1de6b8a683f467
Antonio Esclapez <titoway33@gmail.com>
2024-12-11 09:18:28 +0100

    added button logic

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -5,19 +5,34 @@ import {
   Validators,
 } from '@angular/forms';
 import { ValidationClassesDirective } from '../../shared/directives/validation-classes.directive';
+import { GoogleLoginDirective } from '../../google-login/google-login.directive';
+import { LoadGoogleApiService } from '../../google-login/load-google-api.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'login-page',
   standalone: true,
-  imports: [ReactiveFormsModule, ValidationClassesDirective],
+  imports: [
+    ReactiveFormsModule,
+    ValidationClassesDirective,
+    GoogleLoginDirective,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
   #fb = inject(NonNullableFormBuilder);
 
+  #loadGoogle = inject(LoadGoogleApiService);
+
   loginForm = this.#fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
   });
+
+  constructor() {
+    this.#loadGoogle.credential$.pipe(takeUntilDestroyed()).subscribe(
+      (resp) => console.log(resp.credential) // Envia esto tu API
+    );
+  }
 }
