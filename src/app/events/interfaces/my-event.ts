8a1e164e48a9fee62906f36bf17c70948d8c32b6
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-11 07:29:02 +0100

    Updated interface

--- src/app/events/interfaces/my-event.ts
+++ src/app/events/interfaces/my-event.ts
@@ -1,8 +1,30 @@
-export interface MyEvent {
-  id?: number;
+// export interface MyEvent {
+//   id?: number;
+//   title: string;
+//   description: string;
+//   price: number;
+//   image: string;
+//   date: string;
+// }
+
+import { User } from '../../auth/interfaces/user';
+
+export interface MyEventInsert {
   title: string;
   description: string;
   price: number;
+  lat: number;
+  lng: number;
+  address: string;
   image: string;
   date: string;
+}
+
+export interface MyEvent extends MyEventInsert {
+  id: number;
+  creator: User;
+  distance: number;
+  numAttend: number;
+  attend: boolean;
+  mine: boolean;
 }
