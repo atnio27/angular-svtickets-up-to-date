a3aff4304c37c6126de9f3009876db7ea4c9ab44
Antonio Esclapez <titoway33@gmail.com>
2024-12-11 11:02:13 +0100

    added fb button logic

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -8,6 +8,9 @@ import { ValidationClassesDirective } from '../../shared/directives/validation-c
 import { GoogleLoginDirective } from '../../google-login/google-login.directive';
 import { LoadGoogleApiService } from '../../google-login/load-google-api.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FbLoginDirective } from '../../facebook-login/fb-login.directive';
+// import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+// import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'login-page',
@@ -16,6 +19,8 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     ReactiveFormsModule,
     ValidationClassesDirective,
     GoogleLoginDirective,
+    FbLoginDirective,
+    // FaIconComponent,
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
@@ -34,5 +39,14 @@ export class LoginPageComponent {
     this.#loadGoogle.credential$.pipe(takeUntilDestroyed()).subscribe(
       (resp) => console.log(resp.credential) // Envia esto tu API
     );
+  }
+
+  loggedFacebook(resp: fb.StatusResponse) {
+    // Envía esto a tu API
+    console.log(resp.authResponse.accessToken);
+  }
+
+  showError(error: string) {
+    console.error(error);
   }
 }
