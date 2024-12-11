b348c45f69c0b07deeaa484bdf361b3989c04db6
Antonio Esclapez <titoway33@gmail.com>
2024-12-11 09:19:50 +0100

    directive that creates a google button in a div element

---
+++ src/app/google-login/google-login.directive.ts
@@ -0,0 +1,18 @@
+import { afterNextRender, Directive, ElementRef, inject } from '@angular/core';
+import { LoadGoogleApiService } from './load-google-api.service';
+
+@Directive({
+  selector: '[googleLogin]',
+  standalone: true,
+})
+export class GoogleLoginDirective {
+  #element = inject(ElementRef);
+  #loadService = inject(LoadGoogleApiService);
+
+  constructor() {
+    // Nos aseguramos que no se ejecuta en el lado del servidor si tenemos SSR activado
+    afterNextRender(() =>
+      this.#loadService.setGoogleBtn(this.#element.nativeElement)
+    );
+  }
+}
