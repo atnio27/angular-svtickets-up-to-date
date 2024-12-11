02040b3e2d168e137305471a94092e71ffe116f5
Antonio Esclapez <titoway33@gmail.com>
2024-12-11 12:46:53 +0100

    changed api and started authservice

--- src/app/shared/interceptors/base-url.interceptor.ts
+++ src/app/shared/interceptors/base-url.interceptor.ts
@@ -3,7 +3,7 @@ import { isDevMode } from '@angular/core';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   const serverUrl = isDevMode()
-    ? 'https://api.fullstackpro.es/svtickets-lite'
+    ? 'https://api.fullstackpro.es/svtickets'
     : 'https://localhost:3000';
   const reqClone = req.clone({
     url: `${serverUrl}/${req.url}`,
