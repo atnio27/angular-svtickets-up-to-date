995b64e8b0e5a954ef38b00d11a334c963e5f0fd
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-12 13:05:03 +0100

    Created login fucntion and register function

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { User, UserLogin } from '../interfaces/user';
+import { TokenResponse } from '../interfaces/auth-responses';
+import { map, Observable } from 'rxjs';
+import { UserRegisterResponse } from '../interfaces/user-responses';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  #authUrl = 'auth';
+  #http = inject(HttpClient);
 
-  constructor() { }
+  #logged: WritableSignal<boolean> = signal(false);
+
+  get logged() {
+    return this.#logged;
+  }
+
+  login(user: UserLogin): Observable<void> {
+    const loginUrl = `${this.#authUrl}/login/`;
+
+    return this.#http.post<TokenResponse>(loginUrl, user).pipe(
+      map((resp) => {
+        localStorage.setItem('token', resp.accessToken);
+        this.#logged.set(true);
+      })
+    );
+  }
+
+  register(user: User): Observable<void> {
+    const registerUrl = `${this.#authUrl}/register/`;
+
+    return this.#http.post<UserRegisterResponse>(registerUrl, user).pipe(
+      map((resp) => {
+        console.log(resp.email);
+      })
+    );
+  }
 }
