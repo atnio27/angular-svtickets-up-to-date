0a435c5386c0a914dbb527c32d5291ceb30cb748
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-12 13:04:30 +0100

    Added register logic

--- src/app/auth/register-page/register-page.component.ts
+++ src/app/auth/register-page/register-page.component.ts
@@ -8,6 +8,9 @@ import { ValidationClassesDirective } from '../../shared/directives/validation-c
 import { GeolocationService } from '../services/geolocation.service';
 import { EncodeBase64Directive } from '../../shared/directives/encode-base64.directive';
 import { equalValues } from '../../shared/validators/equal-values.validator';
+import { User } from '../interfaces/user';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'register-page',
@@ -22,12 +25,14 @@ import { equalValues } from '../../shared/validators/equal-values.validator';
 })
 export class RegisterPageComponent {
   #fb = inject(NonNullableFormBuilder);
+  #authService = inject(AuthService);
+  #router = inject(Router);
 
   registerForm = this.#fb.group({
     name: ['', Validators.required],
     password: ['', [Validators.required, Validators.minLength(4)]],
-    latitude: [0],
-    longitude: [0],
+    lat: [0],
+    lng: [0],
     image: ['', [Validators.required]],
   });
 
@@ -47,10 +52,33 @@ export class RegisterPageComponent {
     }
   }
 
+  register() {
+    const registerForm = {
+      ...this.registerForm.getRawValue(),
+      ...this.emailGroupForm.getRawValue(),
+    };
+
+    const user: User = {
+      name: registerForm.name,
+      email: registerForm.email,
+      password: registerForm.password,
+      lat: +registerForm.lat,
+      lng: +registerForm.lng,
+      avatar: this.imageBase64,
+    };
+
+    this.#authService
+      .register(user)
+      .pipe()
+      .subscribe(() => {
+        this.#router.navigate(['/auth/login']);
+      });
+  }
+
   constructor() {
     // geolocation
-    const latitude = this.registerForm.get('latitude');
-    const longitude = this.registerForm.get('longitude');
+    const latitude = this.registerForm.get('lat');
+    const longitude = this.registerForm.get('lng');
 
     GeolocationService.getLocation()
       .then((coords) => {
