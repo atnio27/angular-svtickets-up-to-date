3c29ea1fbc1af16a1602d760533593100bf4fe1d
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-12 13:06:13 +0100

    Updated for better functionality

--- src/app/google-login/google-login.directive.ts
+++ src/app/google-login/google-login.directive.ts
@@ -1,18 +1,35 @@
-import { afterNextRender, Directive, ElementRef, inject } from '@angular/core';
+import {
+  afterNextRender,
+  Directive,
+  ElementRef,
+  inject,
+  output,
+  PLATFORM_ID,
+} from '@angular/core';
 import { LoadGoogleApiService } from './load-google-api.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
-  selector: '[googleLogin]',
+  selector: 'google-login',
   standalone: true,
 })
 export class GoogleLoginDirective {
   #element = inject(ElementRef);
-  #loadService = inject(LoadGoogleApiService);
+  platformId = inject(PLATFORM_ID);
+  // Solo inyectamos el servicio y cargamos la librería si estamos en el cliente (SSR)
+  #loadService = isPlatformBrowser(this.platformId)
+    ? inject(LoadGoogleApiService)
+    : null;
+  login = output<google.accounts.id.CredentialResponse>();
 
   constructor() {
     // Nos aseguramos que no se ejecuta en el lado del servidor si tenemos SSR activado
     afterNextRender(() =>
-      this.#loadService.setGoogleBtn(this.#element.nativeElement)
+      this.#loadService?.setGoogleBtn(this.#element.nativeElement)
     );
+    this.#loadService?.credential$
+      .pipe(takeUntilDestroyed())
+      .subscribe((resp) => this.login.emit(resp));
   }
 }
