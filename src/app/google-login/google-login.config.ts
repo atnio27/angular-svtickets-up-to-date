b4fd3d1f8646c53969953620adc3cb95eccf8d76
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-12 13:05:50 +0100

    minor change

--- src/app/google-login/google-login.config.ts
+++ src/app/google-login/google-login.config.ts
@@ -1,8 +1,6 @@
 import { InjectionToken, Provider } from '@angular/core';
 
 export const CLIENT_ID = new InjectionToken<string>('client_id');
-// Hay que usar aqui la id ??                           ^
-// 540772868802-d260kpth4ucbmu3rhdcj47rc029u54bb.apps.googleusercontent.com
 
 export function provideGoogleId(clientId: string): Provider {
   return { provide: CLIENT_ID, useValue: clientId };
