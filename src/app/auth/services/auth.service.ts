b64e11f9b40410e905ba20c1165e39dfe8ecb4b8
Antonio Esclapez <titoway33@gmail.com>
2024-12-13 11:32:12 +0100

    Updated authentication to cookies

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
-import { User, UserLogin } from '../interfaces/user';
+import { GoogleLogin, User, UserLogin } from '../interfaces/user';
 import { TokenResponse } from '../interfaces/auth-responses';
 import { map, Observable } from 'rxjs';
 import { UserRegisterResponse } from '../interfaces/user-responses';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
 
 @Injectable({
   providedIn: 'root',
@@ -13,18 +14,29 @@ export class AuthService {
   #http = inject(HttpClient);
 
   #logged: WritableSignal<boolean> = signal(false);
+  cookieService = inject(SsrCookieService);
 
   get logged() {
     return this.#logged;
   }
 
   login(user: UserLogin): Observable<void> {
-    const cookieService = inject(SsrCookieService);
     const loginUrl = `${this.#authUrl}/login/`;
 
     return this.#http.post<TokenResponse>(loginUrl, user).pipe(
       map((resp) => {
-        localStorage.setItem('token', resp.accessToken);
+        this.cookieService.set('token', resp.accessToken);
+        this.#logged.set(true);
+      })
+    );
+  }
+
+  googleLogin(googleLogin: GoogleLogin): Observable<void> {
+    const loginUrl = `${this.#authUrl}/google/`;
+
+    return this.#http.post<TokenResponse>(loginUrl, googleLogin).pipe(
+      map((resp) => {
+        this.cookieService.set('token', resp.accessToken);
         this.#logged.set(true);
       })
     );
