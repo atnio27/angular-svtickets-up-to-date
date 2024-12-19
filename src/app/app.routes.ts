ea473e45559505d66b0b69e8fa256ac6751349c7
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-19 19:51:49 +0100

    Added logout and login guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
-import { loginActivateGuard } from './auth/guards/login-activate.guard';
+import { LoginActivateGuard } from './auth/guards/login-activate.guard';
+import { LogoutActivateGuard } from './auth/guards/logout-activate.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [LogoutActivateGuard],
     loadChildren: () => import('./auth/auth.routes').then((m) => m.eventRoutes),
   },
   {
     path: 'events',
+    canActivate: [LoginActivateGuard],
     loadChildren: () =>
       import('./events/event.routes').then((m) => m.eventRoutes),
-    canActivate: [loginActivateGuard],
   },
   {
     path: '',
